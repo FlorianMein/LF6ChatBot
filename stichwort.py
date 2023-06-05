6a8e342eb04eb1b078b345285db8f69cf4f539b2
Tobalix <115449945+Tobalix@users.noreply.github.com>
2023-06-05 12:30:20 +0200

    Update stichwort.py

--- stichwort.py
+++ stichwort.py
@@ -2,17 +2,21 @@
 
 
 # gibt eine Liste aus mit möglichen Problemen
-def whatistopic(string,problemlist):
+def whatistopic(string : str ,department:str ,problemlist:dict):
 
     topiclist = []
     string = string.lower()
     string = string.replace(" ","")
-
-    for x in problemlist:
+    
+    key_list = problemlist[department]
+    for x in key_list:
         # Methode find()
         position = string.find(x)
         if position != -1:
             topiclist.append(x)
+
+    if topiclist == []:
+        return "notFound"
     
     return topiclist
 
